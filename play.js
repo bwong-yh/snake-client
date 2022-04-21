2f2ae5049a86918b51bd8c5de37d1792e2275fe5
Billy Wong <bwong.yh@protonmail.com>
2022-04-22 04:50:17 +0900

    Update connect function is passed in the setInput function for user's input to interaction with the server

--- play.js
+++ play.js
@@ -1,27 +1,6 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting to server...");
 
-// setup input from the user
-const setupInput = function () {
-  const stdin = process.stdin;
-
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", key => handleInput(key));
-
-  return stdin;
-};
-
-// handle input from the user
-const handleInput = function (key) {
-  if (key === "\u0003") process.exit();
-
-  console.log(key);
-};
-
-setupInput();
-
-connect();
+setupInput(connect);
